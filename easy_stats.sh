3f13089b3d1bca08d45ac71d423a5b48c49a8be8
PatrickMogul <patrick@easynode.pro>
2022-11-14 19:48:05 -0600

    ez

--- easy_stats.sh
+++ easy_stats.sh
@@ -6,4 +6,4 @@ curl 'http://localhost:8669/version'; echo
 curl 'http://localhost:8668/version'; echo
 curl 'http://localhost:8667/version'; echo
 
-echo "Stats provided by EasyNode.PRO."
+echo "Stats provided by EasyNode.PRO"
