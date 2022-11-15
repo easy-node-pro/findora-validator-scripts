bbcbd36d750cae8cc4529dfa119ce46d7bcb658b
PatrickMogul <patrick@easynode.pro>
2022-11-14 19:10:54 -0600

    stats.sh

---
+++ easy_stats.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+USERNAME=$USER
+
+curl 'http://localhost:26657/status'; echo
+curl 'http://localhost:8669/version'; echo
+curl 'http://localhost:8668/version'; echo
+curl 'http://localhost:8667/version'; echo
+
+echo "Local node updated and restarted."
