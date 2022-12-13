3c460d29b3fd641f1b06bc2f34539a5005ec9a60
PatrickMogul <patrick@easynode.pro>
2022-12-13 10:48:33 -0600

    better check

--- easy_update_testnet.sh
+++ easy_update_testnet.sh
@@ -6,7 +6,7 @@ NAMESPACE=testnet
 LIVE_VERSION=$(curl -s https://${ENV}-${NAMESPACE}.${ENV}.findora.org:8668/version | awk -F\  '{print $2}')
 FINDORAD_IMG=findoranetwork/findorad:${LIVE_VERSION}
 export ROOT_DIR=/data/findora/${NAMESPACE}
-CONTAINER_NAME='findorad'
+CONTAINER_NAME=findorad
 
 # Reset permissions to avoid problems.
 sudo chown -R ${USERNAME}:${USERNAME} ${ROOT_DIR}
@@ -14,7 +14,7 @@ sudo chown -R ${USERNAME}:${USERNAME} ${ROOT_DIR}
 ###################
 # Stop local node #
 ###################
-if docker ps -a --format '{{.Names}}' | grep -Eq "^${CONTAINER_NAME}\$"; then
+if docker ps -a --format '{{.Names}}' | grep -Eq ${CONTAINER_NAME}; then
   echo -e "Findorad Container found, stopping container to restart."
   docker stop findorad
   docker rm findorad 
